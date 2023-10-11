bf29607737d1174a744b0977aee8077524d569b2
Chirag RV <yt9196>
2023-10-11 22:20:26 +0530

    create mehod controller

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -7,126 +7,90 @@ use App\Models\item;
 use App\Models\order;
 use App\Models\tax;
 
-class orderController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    class orderController extends Controller
     {
-        // Retrieve a list of orders
-        // $orders = order::all();
-        // return view('orders.index', ['orders' => $orders]);
-    }
-
-    /** 
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-        public function createOrder(Request $request)
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
         {
-            // Validate the request data
-            $request->validate([
-                'order_data' => 'required|array',
-                'order_data.items' => 'required|array|min:1',
-                'order_data.items.*.name' => 'required|string',
-                'order_data.items.*.quantity' => 'required|integer|min:1',
-                'order_data.items.*.price' => 'required|numeric|min:0',
-                'order_data.items.*.taxes' => 'array',
-                'order_data.items.*.taxes.*.name' => 'required|string',
-                'order_data.items.*.taxes.*.percentage' => 'required|numeric|min:0',
+            // Retrieve a list of orders
+            // $orders = order::all();
+            // return view('orders.index', ['orders' => $orders]);
+        }
+
+        /** 
+         * Show the form for creating a new resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function create()
+        {
+            return view('create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            $validatedData = $request->validate([
+                'orderNo' => 'required',
+                'orderDate' => 'required|date',
             ]);
-    
-            // Create a new order
-            $order = order::create();
-    
-            // Add items to the order
-            foreach ($request->order_data['items'] as $itemData) {
-                $item = new item([
-                    'name' => $itemData['name'],
-                    'quantity' => $itemData['quantity'],
-                    'price' => $itemData['price'],
-                ]);
-                
-                // Save the item to the order
-                $order->items()->save($item);
-    
-                // Add taxes to the item
-                if (isset($itemData['taxes']) && is_array($itemData['taxes'])) {
-                    foreach ($itemData['taxes'] as $taxData) {
-                        $tax = new tax([
-                            'name' => $taxData['name'],
-                            'percentage' => $taxData['percentage'],
-                        ]);
-    
-                        // Save the tax to the item
-                        $item->taxes()->save($tax);
-                    }
-                }
-            }
-            // Return a response or redirect as needed
-            return response()->json(['message' => 'Order created successfully']);
-        }  
+        
+            Order::create($validatedData);
+        
+            return redirect()->route('orders.create')->with('success', 'Order created successfully.');
+        }
+        /**
+         * Display the specified resource.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function show($id)
+        {
+            // $order = Order::find($id);
+            // return view('orders.show', ['order' => $order]);
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // Store a new order in the database
-        // order::create($request->all());
-        // return redirect()->route('orders.index');
-    }
+        /**
+         * Show the form for editing the specified resource.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function edit($id)
+        {
+            //
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        // $order = Order::find($id);
-        // return view('orders.show', ['order' => $order]);
-    }
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request, $id)
+        {
+            //
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy($id)
+        {
+            //
+        }
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
